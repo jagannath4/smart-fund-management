32a276075a4b33fc19c7c649d1e35634aae33859
JAGANNATH <99709081+jagannath4@users.noreply.github.com>
2025-03-30 15:38:43 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",  // Enables static export
+  basePath: "/smart-fund-management",  // Change this to your repo name
+  trailingSlash: true,
+};
+
+export default nextConfig;
