0f2c1c3eee03327345ac0a336639c1aad9a7b305
JAGANNATH <99709081+jagannath4@users.noreply.github.com>
2025-03-30 15:35:03 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',  // This enables static export
-  basePath: '/smart-fund-management', // Change this to your repo name
-  trailingSlash: true,
-};
-
-module.exports = nextConfig;
